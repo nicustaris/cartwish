718b3fd8485aa8cacab599cfea58f84c00b4a99f
nicustaris <nicolae.staris@gmail.com>
2023-11-19 18:27:30 +0000

    link update

--- src/components/Home/Homepage.jsx
+++ src/components/Home/Homepage.jsx
@@ -11,14 +11,14 @@ const Homepage = () => {
       <Herosection
         title="iPhone 14 Pro"
         subtitle="Unleash the future with the iPhone 15 - a perfect blend of style, power, and innovation in a single device."
-        link="/product/6538494488ed7058951f07d9"
+        link="/product/65581081bf3f55531794ddbc"
         image={iphone}
       />
       <Featuredproducts />
       <Herosection
         title="MacBook Pro 16-inch with M1 Pro/M1 Max"
         subtitle="Elevate your productivity with the powerful MacBook Pro 16-inch, featuring Apple's M1 Pro/M1 Max chips for exceptional performance and stunning visuals. Experience the future of computing."
-        link="/product/6538494488ed7058951f07e1"
+        link="/product/65581081bf3f55531794ddc4"
         image={macbook}
       />
     </div>
